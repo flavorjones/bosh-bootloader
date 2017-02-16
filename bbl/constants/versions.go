fee530820c3756447b193caab132a3325addf1fc
Mike Dalessio <mike.dalessio@gmail.com>
2017-02-16 16:56:09 -0500

    bosh 261 on gcp stemcell 3312.18

--- bbl/constants/versions.go
+++ bbl/constants/versions.go
@@ -3,14 +3,20 @@ package constants
 // THIS FILE IS GENERATED AUTOMATICALLY, NO TOUCHING!!!!!
 
 const (
-	BOSHURL         = "https://s3.amazonaws.com/bbl-precompiled-bosh-releases/release-bosh-260.5-on-ubuntu-trusty-stemcell-3312.17.tgz"
-	BOSHSHA1        = "2954125498eb400b06d66f973bd017777cfa640a"
+//BOSHURL         = "https://s3.amazonaws.com/bbl-precompiled-bosh-releases/release-bosh-260.5-on-ubuntu-trusty-stemcell-3312.17.tgz"
+//BOSHSHA1        = "2954125498eb400b06d66f973bd017777cfa640a"
+	BOSHURL         = "https://s3.amazonaws.com/bbl-precompiled-bosh-releases/release-bosh-261-on-ubuntu-trusty-stemcell-3312.18.tgz"
+	BOSHSHA1        = "d86b9ba0e78cc311f1f3a46f0ea2b2103011e0bf"
+
 	BOSHAWSCPIURL   = "https://bosh.io/d/github.com/cloudfoundry-incubator/bosh-aws-cpi-release?v=62"
 	BOSHAWSCPISHA1  = "f36967927ceae09e5663a41fdda199edfe649dc6"
 	AWSStemcellURL  = "https://s3.amazonaws.com/bosh-aws-light-stemcells/light-bosh-stemcell-3312.17-aws-xen-hvm-ubuntu-trusty-go_agent.tgz"
 	AWSStemcellSHA1 = "fa6908fee576ac86222a43700ce19c40051be403"
 	BOSHGCPCPIURL   = "https://bosh.io/d/github.com/cloudfoundry-incubator/bosh-google-cpi-release?v=25.6.2"
 	BOSHGCPCPISHA1  = "b4865397d867655fdcc112bc5a7f9a5025cdf311"
-	GCPStemcellURL  = "https://s3.amazonaws.com/bosh-gce-light-stemcells/light-bosh-stemcell-3312.17-google-kvm-ubuntu-trusty-go_agent.tgz"
-	GCPStemcellSHA1 = "b788e7b08dccec03515ef3b4a2e2227dc3f98d55"
+
+//GCPStemcellURL  = "https://s3.amazonaws.com/bosh-gce-light-stemcells/light-bosh-stemcell-3312.17-google-kvm-ubuntu-trusty-go_agent.tgz"
+//GCPStemcellSHA1 = "b788e7b08dccec03515ef3b4a2e2227dc3f98d55"
+	GCPStemcellURL  = "https://s3.amazonaws.com/bosh-gce-light-stemcells/light-bosh-stemcell-3312.18-google-kvm-ubuntu-trusty-go_agent.tgz"
+	GCPStemcellSHA1 = "a0f19414d61deff1bf6439bb1598bd6a732712df"
 )
